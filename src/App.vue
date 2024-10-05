4bb7bb19b63a1fdc4b357bd0a5fdfd9fc6a6b26c
songsong.li <songsong.li@pinlandata.com>
2024-10-05 09:13:11 +0800

    feat: npm install @element-plus/icons-vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
+<script setup lang="ts">
+import { Delete, Edit, Loading, Search, Share } from '@element-plus/icons-vue';
+</script>
+
 <template>
   <p>
   通过添加额外的类名 <b>is-loading</b>，你的图标就可以在 2 秒内旋转 360
   度，但让你也可以自己改写想要的动画。
 </p>
 <el-icon :size="20">
-  <edit />
+  <Edit />
 </el-icon>
 <el-icon color="#409EFC" class="no-inherit">
-  <share />
+  <Share />
 </el-icon>
 <el-icon>
-  <delete />
+  <Delete />
 </el-icon>
 <el-icon class="is-loading">
-  <loading />
+  <Loading />
 </el-icon>
 <el-button type="primary">
   <el-icon style="vertical-align: middle;">
-    <search />
+    <Search />
   </el-icon>
   <span style="vertical-align: middle;"> 搜索 </span>
 </el-button>
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
-import * as Icons from '@element-plus/icons'
 
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
-Object.keys(Icons).forEach(key => {
-    app.component(key, Icons[key])
-})
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+app.component(key, component)
+}
 
 app.use(ElementPlus)
 app.mount('#app')
