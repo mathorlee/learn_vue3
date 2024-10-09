c22ab63ddc6ab659e5aae689fa48badc0dc80e01
songsong.li <songsong.li@pinlandata.com>
2024-10-09 14:57:43 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,79 @@
 <template>
-  <!-- 各种类型的按钮 -->
-  <p>Basic text button</p>
-  <div class="mb-4">
-    <el-button 
-    v-for="button in buttons"
-    :key="button.text"
-    :type="button.type"
-    size="large"
-    >
-    {{ button.text }}
-    </el-button>
-  </div>
-
-  <p>Background color always on</p>
-  <div class="mb-4">
-    <el-button
-    v-for="button in buttons"
-    :type="button.type"
-    size="medium"
-    text
-    bg
-    >
-    {{ button.text }}
-    </el-button>
-  </div>
-
-  <p>Disabled text button</p>
-  <div class="mb-4">
-    <el-button
-    v-for="button in buttons"
-    :type="button.type"
-    disabled
-    text
-    size="small"
-    >
-    {{ button.text }}
-    </el-button>
-  </div>
-
-  <!-- icon按钮 -->
-  <div class="mb-4">
-    <el-button type="primary" :icon="Edit">Edit</el-button>
-    <el-button type="primary" :icon="Share">Share</el-button>
-    <el-button type="primary" :icon="Delete">Delete</el-button>
-    <el-button type="primary" :icon="Search">Search</el-button>
-    <el-button type="primary" :icon="Upload">Upload</el-button>
-    <el-button type="primary">
-      Upload
-      <el-icon class="el-icon--right">
-        <Upload/>
-      </el-icon>
-    </el-button>
-  </div>
-
-  <el-button-group>
-    <el-button type="primary" :icon="ArrowLeft">Previous Page</el-button>
-    <el-button type="primary">
-      Next Page<el-icon class="el-icon--right"><ArrowRight /></el-icon>
-    </el-button>
-  </el-button-group>
-
-  <el-button-group class="ml-4">
-    <el-button type="primary" :icon="Edit" />
-    <el-button type="primary" :icon="Share" />
-    <el-button type="primary" :icon="Delete" />
-  </el-button-group>
-
-
-
+  <el-form ref="form" :model="form" label-width="80px">
+  <el-form-item label="活动名称">
+    <el-input v-model="form.name"></el-input>
+  </el-form-item>
+  <el-form-item label="活动区域">
+    <el-select v-model="form.region" placeholder="请选择活动区域">
+      <el-option label="区域一" value="shanghai"></el-option>
+      <el-option label="区域二" value="beijing"></el-option>
+    </el-select>
+  </el-form-item>
+  <el-form-item label="活动时间">
+    <el-col :span="11">
+      <el-date-picker
+        type="date"
+        placeholder="选择日期"
+        v-model="form.date1"
+        style="width: 100%;"
+      ></el-date-picker>
+    </el-col>
+    <el-col class="line" :span="2">-</el-col>
+    <el-col :span="11">
+      <el-time-picker
+        placeholder="选择时间"
+        v-model="form.date2"xx`
+        style="width: 100%;"
+      ></el-time-picker>
+    </el-col>
+  </el-form-item>
+  <el-form-item label="即时配送">
+    <el-switch v-model="form.delivery"></el-switch>
+  </el-form-item>
+  <el-form-item label="活动性质">
+    <el-checkbox-group v-model="form.type">
+      <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
+      <el-checkbox label="地推活动" name="type"></el-checkbox>
+      <el-checkbox label="线下主题活动" name="type"></el-checkbox>
+      <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+    </el-checkbox-group>
+  </el-form-item>
+  <el-form-item label="特殊资源">
+    <el-radio-group v-model="form.resource">
+      <el-radio label="线上品牌商赞助"></el-radio>
+      <el-radio label="线下场地免费"></el-radio>
+    </el-radio-group>
+  </el-form-item>
+  <el-form-item label="活动形式">
+    <el-input type="textarea" v-model="form.desc"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="onSubmit">立即创建</el-button>
+    <el-button>取消</el-button>
+  </el-form-item>
+</el-form>
 </template>
 
-<script setup lang="ts">
-import {Edit, Share, Delete, Search, Upload, ArrowLeft, ArrowRight} from "@element-plus/icons-vue"
-
-const buttons = [
-  {type: "plain", text: "plain"},
-  {type: "primary", text: "primary"},
-  {type: "success", text: "success"},
-  {type: "info", text: "info"},
-  {type: "warning", text: "warning"},
-  {type: "danger", text: "danger"},
-]
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: '',
+        },
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!')
+      },
+    },
+  }
 </script>
