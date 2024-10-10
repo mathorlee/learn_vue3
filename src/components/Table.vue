127499f9cb9d2c712acc7eb4277cfc5505b4086c
songsong.li <songsong.li@pinlandata.com>
2024-10-10 14:40:25 +0800

    feat: add table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,9 +1,66 @@
 <template>
-  <li>xxx</li>
+  <el-table 
+  ref="tableView"
+  :data="rows" 
+  stripe 
+  border 
+  highlight-current-row 
+  height="150"
+  @current-change="onChange"
+  >
+    <el-table-column fixed prop="date" width="200" label="日期"></el-table-column>
+    <el-table-column prop="name" width="200" label="姓名"></el-table-column>
+    <el-table-column prop="address" width="400" label="地址"></el-table-column>
+  </el-table>
+
+  <div style="margin-top: 20px;">
+    <el-button @click="doSelect(rows[1])">选中第二行</el-button>
+    <el-button @click="cancleSelect()">取消选择</el-button>
+  </div>
 </template>
 
 <script setup>
-const props = defineProps({
-  todo: Object
-})
+import { ref, getCurrentInstance } from "vue";
+
+// vue3中获取当前组件的实例对象：https://blog.csdn.net/m0_62811051/article/details/128121082
+const view = getCurrentInstance();
+
+const rows = ref([
+  {
+    date: '2016-05-02',
+    name: '王小虎',
+    address: '上海市普陀区金沙江路 1518 弄',
+  },
+  {
+    date: '2016-05-02',
+    name: '张三',
+    address: '上海市普陀区金沙江路 1518 弄',
+  },
+  {
+    date: '2016-05-02',
+    name: '李四',
+    address: '上海市普陀区金沙江路 1518 弄',
+  },
+  {
+    date: '2016-05-02',
+    name: '王五',
+    address: '上海市普陀区金沙江路 1518 弄',
+  },
+])
+
+const currentRow = ref("")
+
+function onChange(val) {
+  currentRow.value = val
+  console.log(currentRow.value)
+}
+
+function doSelect(val) {
+  console.log(val)
+  view.refs.tableView.setCurrentRow(val)
+}
+
+function cancleSelect() {
+  view.refs.tableView.setCurrentRow(null)
+}
 </script>
